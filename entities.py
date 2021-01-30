3f91da8c99251d1d6f84145343bc4cf4f0827bed
Sawyer <sawyercoletang@gmail.com>
2021-01-30 17:36:27 -0500

    updated Atom obj

--- entities.py
+++ entities.py
@@ -1,14 +1,23 @@
+from mendeleev import element
 
-# from mendeleev import element
 
 class Atom:
-    def __init__(self):
-        self.number = None # atomic number
-        self.neighbours = None # list of integer atom indexes
-        self.hidden_charge = None  # int
-        self.formal_charge = None  # int
+    def __init__(self, symbol: str, neighbours: list):
+        self.symbol = symbol
+        self.number = element(symbol).atomic_number
+        self.neighbours = neighbours  # list of integer atom indexes
+
+    def get_symbol(self) -> str:
+        return self.symbol
+
+    def get_number(self) -> int:
+        return self.number
+
+    def get_neighbours(self) -> list:
+        return self.neighbours
+
 
 class resonanceStruct:
     def __init__(self):
-        self.atoms = None # list of atom
-        self.double_bonds = None #list of tuples(atom index, atom index)
+        self.atoms = None  # list of atom
+        self.double_bonds = None  # list of tuples(atom index, atom index)
