05568e32d57d0ee9e3f20f873ddc2d06211a899d
Sawyer <sawyercoletang@gmail.com>
2021-01-30 18:20:57 -0500

    added more fields to Atom obj

--- entities.py
+++ entities.py
@@ -1,11 +1,12 @@
-from mendeleev import element
+from mendeleev import element  # `pip install mendeleev`
 
 
 class Atom:
-    def __init__(self, symbol: str, neighbours: list):
+    def __init__(self, symbol: str, neighbours: list, lone_pairs: int):
         self.symbol = symbol
         self.number = element(symbol).atomic_number
         self.neighbours = neighbours  # list of integer atom indexes
+        self.valence_electrons = len(self.neighbours) + lone_pairs*2  # number of valence electrons on the atom.
 
     def get_symbol(self) -> str:
         return self.symbol
@@ -15,6 +16,12 @@ class Atom:
 
     def get_neighbours(self) -> list:
         return self.neighbours
+
+    def get_sigma_bonds(self) -> int:
+        return len(self.neighbours)
+
+    def get_valence_electrons(self) -> int:
+        return self.valence_electrons
 
 
 class resonanceStruct:
