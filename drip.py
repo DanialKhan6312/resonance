77f91fcf5b3d5c93d5e2eb2c2db36bd25c6a53d2
r53lin <78062256+r53lin@users.noreply.github.com>
2021-01-30 23:10:54 -0500

    Update drip.py

--- drip.py
+++ drip.py
@@ -1,1 +1,8 @@
+import cv2
+import numpy as np
 
+s_img = cv2.imread("C:/Users/Raymond/Downloads/drip.png")
+l_img = cv2.imread("C:/Users/Raymond/Downloads/face.jpg") 
+x_offset = y_offset = 50
+l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
+cv2.imshow('drip',l_img)
