f1ba906452d4cd8dbe0440e1db0c838f64f2b94c
r53lin <78062256+r53lin@users.noreply.github.com>
2021-01-30 23:55:46 -0500

    Update drip.py

--- drip.py
+++ drip.py
@@ -1,8 +1,21 @@
 import cv2
 import numpy as np
 
-s_img = cv2.imread("C:/Users/Raymond/Downloads/drip.png")
-l_img = cv2.imread("C:/Users/Raymond/Downloads/face.jpg") 
-x_offset = y_offset = 50
-l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
-cv2.imshow('drip',l_img)
+drip = cv2.imread("C:/Users/Raymond/Downloads/drip.png")
+face = cv2.imread("C:/Users/Raymond/Downloads/face.jpg") 
+
+
+scale_percent = 80
+width = int(drip.shape[1] * scale_percent / 100)
+height = int(drip.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+drip = cv2.resize(drip, dim, interpolation = cv2.INTER_AREA)
+
+x_offset = 150
+y_offset = 300
+face[y_offset:y_offset + drip.shape[0], x_offset:x_offset + drip.shape[1]] = drip
+
+cv2.imshow('drip',face)
+cv2.waitKey(0)
+cv2.destroyAllWindows
